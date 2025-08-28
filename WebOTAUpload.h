8e7671a671034c5438f8344a257c1fbf07d8c654
Jan Kieseler <jkiesele@cern.ch>
2025-08-28 22:04:28 +0200

    vector web settings

--- WebOTAUpload.h
+++ WebOTAUpload.h
@@ -6,6 +6,7 @@
 #include <LoggingBase.h>   // uses global gLogger
 #include <esp_ota_ops.h>
 #include <WebItem.h>       // provides virtual access to setupRoutes and generateHTML (BasicWebInterface)
+#include "passwords.h"   // for default password
 
 // Provide a password validator hook so you can read from NVS/SettingsBlockBase.
 using OTAPasswordValidator = std::function<bool(const String& pw)>;
@@ -13,7 +14,7 @@ using OTAPasswordValidator = std::function<bool(const String& pw)>;
 class WebOTAUpload : public WebItem {
 public:
     // Fixed password (simple)
-    WebOTAUpload(const String& password, const String& route = "/otaupdate");
+    WebOTAUpload(const String& password = secret::otaPassword, const String& route = "/otaupdate");
 
     // Dynamic validator (e.g. read from settings)
     WebOTAUpload(OTAPasswordValidator validator, const String& route = "/otaupdate");
--- WebSettings.h
+++ WebSettings.h
@@ -52,6 +52,9 @@ struct SettingBase {
         }
         precision = prec;
     }
+
+    virtual void appendHTMLInputs(String& html) const = 0;
+    virtual void onPost(WebServer& srv) = 0;
 };
 
 template<typename T>
@@ -107,6 +110,22 @@ public:
         else if (valueType == TYPE_INT)  p.putInt  (key, (int)value);
         else                             p.putFloat(key, value);
     }
+
+    void appendHTMLInputs(String& html) const override {
+      if      (valueType == TYPE_BOOL) {
+        html += "<input type='checkbox' name='" + String(key) + "' value='1' ";
+        html += (value ? "checked " : "");
+        html += ">\n";
+      } else if (valueType == TYPE_STRING) {
+        html += "<input type='text' name='" + String(key) + "' value='" + toString() + "'>\n";
+      } else {
+        html += "<input type='text' inputmode='decimal' name='" + String(key) + "' value='" + toString() + "'>\n";
+      }
+    }
+    void onPost(WebServer& srv) override {
+      if (srv.hasArg(key)) fromString(srv.arg(key));
+      else if (valueType == TYPE_BOOL) value = false; // unchecked checkbox
+    }
 };
 
 /*------------------------------------------------------------*/
@@ -145,6 +164,13 @@ public:
     }
     void save(Preferences& p) const override {
         p.putString(key, value);
+    }
+
+    void appendHTMLInputs(String& html) const override {
+       html += "<input type='text' name='" + String(key) + "' value='" + toString() + "'>\n";
+    }
+    void onPost(WebServer& srv) override {
+      if (srv.hasArg(key)) fromString(srv.arg(key));
     }
 };
 
@@ -208,19 +234,8 @@ public:
     
         for (auto* s : registry) {
             html += String(s->label) + ": ";
-            if (s->valueType == SettingBase::TYPE_BOOL) {
-                auto* b = static_cast<Setting<bool>*>(s);
-                html += "<input type='checkbox' name='" + String(s->key) +
-                        "' value='1' " + (b->value ? "checked " : "") +
-                        "><br>\n";
-            } else if (s->valueType == SettingBase::TYPE_STRING) {          // NEW
-                html += "<input type='text' name='" + String(s->key) +
-                        "' value='" + s->toString() + "'><br>\n";
-            } else {
-                html += "<input type='text' inputmode='decimal' name='" +
-                        String(s->key) + "' value='" + s->toString() +
-                        "'><br>\n";
-            }
+            s->appendHTMLInputs(html);
+            html += "<br>\n";
         }
         if (!WebAuthPlugin::instance().isActive()) {
             html += "Password: <input type='password' name='pw'><br><br>\n";
@@ -235,14 +250,7 @@ public:
 private:
     void handlePost(WebServer& srv)
     {
-        for (auto* s : registry) {
-            if (srv.hasArg(s->key)) {
-                s->fromString(srv.arg(s->key));
-            } else if (s->valueType == SettingBase::TYPE_BOOL) {
-                // unchecked checkbox → false
-                static_cast<Setting<bool>*>(s)->value = false;
-            }
-        }
+        for (auto* s : registry) s->onPost(srv);
         if (!sanityCheck()) {
             gLogger->println("Settings: " + String(urlPath) + " sanity check failed (values might have been changed)");
             return;
@@ -262,31 +270,206 @@ private:
 #define DEF_SETTING(TYPE, NAME, LABEL, DEFAULT_VAL, STEP) \
     Setting<TYPE> NAME{ *this, #NAME, LABEL, DEFAULT_VAL, STEP }
 
-/* ===========================================================
-   6.  Example usage                                         *
-   ===========================================================*/
-/*
-class HydroSettings : public SettingsBlockBase {
-public:
-    HydroSettings() : SettingsBlockBase("hydro", "/hydro") {}
+/// Array types
+/***************  Dynamic Array Support  *********************/
+/* Primary IO template (must come before specializations) */
+template<typename T, typename Enable = void>
+struct SettingArrayIO {};
 
-    DEF_SETTING(float, pumpA_relVol,
-                "Parts nutrient pump A", 1.0f, 0.01f);
-    DEF_SETTING(float, mlPerDeltaEC,
-                "Nutrients per ΔEC [ml/mS]", 0.01f, 0.01f);
-    DEF_SETTING(float, targetEC,
-                "Target EC [mS]", 1.20f, 0.01f);
-    DEF_SETTING(bool,  dryRun,
-                "Dry-run mode", true, 1);
+/* floats */
+template<typename T>
+struct SettingArrayIO<T, typename std::enable_if<std::is_floating_point<T>::value>::type> {
+  static T load(Preferences& p, const char* k, T def) { return (T)p.getFloat(k, (float)def); }
+  static void save(Preferences& p, const char* k, T v){ p.putFloat(k, (float)v); }
+  static String toStr(const T& v, unsigned precision){ return String((float)v, precision); }
+  static T fromStr(const String& s){ return (T)s.toFloat(); }
+  static void appendInput(String& html, const String& name, const String& val, bool /*checked*/) {
+    html += "<input type='text' inputmode='decimal' name='" + name + "' value='" + val + "'>";
+  }
 };
 
-class LightingSettings : public SettingsBlockBase {
-public:
-    LightingSettings() : SettingsBlockBase("light", "/light") {}
-
-    DEF_SETTING(int,   dayLength,
-                "Day length [h]", 16, 1);
-    DEF_SETTING(float, dawnFade,
-                "Dawn fade [min]", 30.0f, 0.1f);
+/* integral (except bool) */
+template<typename T>
+struct SettingArrayIO<T, typename std::enable_if<std::is_integral<T>::value && !std::is_same<T,bool>::value>::type> {
+  static T load(Preferences& p, const char* k, T def) { return (T)p.getInt(k, (int)def); }
+  static void save(Preferences& p, const char* k, T v){ p.putInt(k, (int)v); }
+  static String toStr(const T& v, unsigned){ return String((int)v); }
+  static T fromStr(const String& s){ return (T)s.toInt(); }
+  static void appendInput(String& html, const String& name, const String& val, bool /*checked*/) {
+    html += "<input type='text' inputmode='decimal' name='" + name + "' value='" + val + "'>";
+  }
 };
-*/
+
+/* bool */
+template<typename T>
+struct SettingArrayIO<T, typename std::enable_if<std::is_same<T,bool>::value>::type> {
+  static T load(Preferences& p, const char* k, T def) { return p.getBool(k, (bool)def); }
+  static void save(Preferences& p, const char* k, T v){ p.putBool(k, (bool)v); }
+  static String toStr(const T& v, unsigned){ return v ? "1" : "0"; }
+  static T fromStr(const String& s){ return (s == "1" || s == "true" || s == "on"); }
+  static void appendInput(String& html, const String& name, const String& /*val*/, bool checked) {
+    html += "<input type='checkbox' name='" + name + "' value='1' ";
+    html += (checked ? "checked " : "");
+    html += ">";
+  }
+};
+
+/* Arduino String */
+template<typename T>
+struct SettingArrayIO<T, typename std::enable_if<std::is_same<T, String>::value>::type> {
+  static T load(Preferences& p, const char* k, const T& def) { return p.getString(k, def); }
+  static void save(Preferences& p, const char* k, const T& v){ p.putString(k, v); }
+  static String toStr(const T& v, unsigned){ return v; }
+  static T fromStr(const String& s){ return s; }
+  static void appendInput(String& html, const String& name, const String& val, bool /*checked*/) {
+    html += "<input type='text' name='" + name + "' value='" + htmlEscape_(val) + "'>";
+  }
+private:
+  static String htmlEscape_(const String& s) {
+    String out; out.reserve(s.length()+8);
+    for (size_t i=0;i<s.length();++i) {
+      char c=s[i];
+      if (c=='&') out += "&amp;";
+      else if (c=='<') out += "&lt;";
+      else if (c=='>') out += "&gt;";
+      else if (c=='"') out += "&quot;";
+      else out += c;
+    }
+    return out;
+  }
+};
+
+
+/***************  SettingDynArray<T>  ************************/
+
+template<typename T>
+class SettingDynArray : public SettingBase {
+public:
+  std::vector<T> value;
+
+  /* ctor: fixed length + fill default */
+  template<class Block>
+  SettingDynArray(Block& owner,
+                  const char* k,
+                  const char* lbl,
+                  size_t len,
+                  T fillDefault,
+                  float st = 0.01f)
+  {
+    key   = k;
+    label = lbl;
+    step  = st;
+    setPrecisionFromStep(step);
+    setValueType_();
+    value.assign(len, fillDefault);
+    owner.registerSetting(this);
+    default_ = fillDefault;  // remember for resizing
+  }
+
+  /* ctor: explicit defaults (initializer list) */
+  template<class Block>
+  SettingDynArray(Block& owner,
+                  const char* k,
+                  const char* lbl,
+                  std::initializer_list<T> initVals,
+                  float st = 0.01f)
+  {
+    key   = k;
+    label = lbl;
+    step  = st;
+    setPrecisionFromStep(step);
+    setValueType_();
+    value.assign(initVals.begin(), initVals.end());
+    owner.registerSetting(this);
+    
+  }
+
+  void ensureSize(size_t len) {
+    if (value.size() != len){
+        value.assign(len, default_);
+        gLogger->println("Settings: array " + String(key) + " resized to " + String((unsigned)len));
+    } 
+  }
+  /*access to elements */
+  T& operator[](size_t i) {if (i < value.size()) return value[i]; gLogger->println("Settings: array index out of range for " + String(key)); return default_; }
+  const T& operator[](size_t i) const { if (i < value.size()) return value[i]; gLogger->println("Settings: array index out of range for " + String(key)); return default_; }
+
+  /* SettingBase interface not used for arrays as a whole */
+  void   fromString(const String&) override {}
+  String toString() const override { return String(); }
+
+  /* NVS I/O: one key per element: key_0, key_1, ... */
+  void load(Preferences& p) override {
+    const size_t N = value.size();
+    for (size_t i = 0; i < N; ++i) {
+      String ki = String(key) + "_" + String((unsigned)i);
+      value[i] = SettingArrayIO<T>::load(p, ki.c_str(), value[i]);
+    }
+  }
+  void save(Preferences& p) const override {
+    const size_t N = value.size();
+    for (size_t i = 0; i < N; ++i) {
+      String ki = String(key) + "_" + String((unsigned)i);
+      SettingArrayIO<T>::save(p, ki.c_str(), value[i]);
+    }
+  }
+
+  /* HTML rendering */
+  void appendHTMLInputs(String& html) const override {
+    const size_t N = value.size();
+    html += "<span>";
+    for (size_t i = 0; i < N; ++i) {
+      const String fname = String(key) + "_" + String((unsigned)i);
+      const String sval  = SettingArrayIO<T>::toStr(value[i], precision);
+      SettingArrayIO<T>::appendInput(html, fname, sval, checked_(i));
+      html += " ";
+    }
+    html += "</span>";
+  }
+
+  /* POST handling */
+  void onPost(WebServer& srv) override {
+    const size_t N = value.size();
+    for (size_t i = 0; i < N; ++i) {
+      String fname = String(key) + "_" + String((unsigned)i);
+      if (srv.hasArg(fname)) {
+        value[i] = SettingArrayIO<T>::fromStr(srv.arg(fname));
+      } else {
+        setMissing_(i); // only flips to false for bool; no-op for others
+      }
+    }
+  }
+
+private:
+  T default_;
+  void setValueType_() {
+    if      (std::is_same<T,bool>::value)    valueType = TYPE_BOOL;
+    else if (std::is_same<T, String>::value) valueType = TYPE_STRING;
+    else if (std::is_integral<T>::value)     valueType = TYPE_INT;
+    else                                     valueType = TYPE_FLOAT;
+  }
+
+  /* SFINAE: checkbox state only for bool */
+  template<typename U=T>
+  typename std::enable_if<std::is_same<U,bool>::value, bool>::type
+  checked_(size_t i) const { return (bool)value[i]; }
+
+  template<typename U=T>
+  typename std::enable_if<!std::is_same<U,bool>::value, bool>::type
+  checked_(size_t) const { return false; }
+
+  /* SFINAE: set false on missing POST only for bool */
+  template<typename U=T>
+  typename std::enable_if<std::is_same<U,bool>::value, void>::type
+  setMissing_(size_t i) { value[i] = false; }
+
+  template<typename U=T>
+  typename std::enable_if<!std::is_same<U,bool>::value, void>::type
+  setMissing_(size_t) { /* no-op */ }
+};
+
+#define DEF_SETTING_ARRAY(TYPE, NAME, LABEL, DEFAULT_VAL, STEP, LENGTH) \
+  SettingDynArray<TYPE> NAME{ *this, #NAME, LABEL, (size_t)(LENGTH), (TYPE)(DEFAULT_VAL), STEP }
+
+#define DEF_SETTING_ARRAY_INIT(TYPE, NAME, LABEL, STEP, ...) \
+  SettingDynArray<TYPE> NAME{ *this, #NAME, LABEL, std::initializer_list<TYPE>{ __VA_ARGS__ }, STEP }
